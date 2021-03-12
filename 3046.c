d69ffb39b834d2aece66fc407e677af07fddd592
Gasiyori <75403856+Gasiyori@users.noreply.github.com>
2021-03-12 19:22:34 +0900

    Add files via upload

---
+++ 3046.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+#pragma warning (disable : 4996)
+
+int main() {
+
+	int R1, S;
+
+	scanf("%d %d", &R1, &S);
+
+	printf("%d", 2 * S - R1);
+}
+
+
+//S = (R1+R2)/2
+
+//S = R1 / 2 + R2 / 2
+
+//R1과 S를 기억할때 R2가 몇 인지 구하시오.
